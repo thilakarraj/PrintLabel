d6b9970b1f0dfd903636fe3516e894a446808f16
thilakarraj <thilakarraj.s@justintegrateit.com>
2015-11-23 19:16:40 +0530

    contanst file commited

---
+++ PrinterScheduler/src/com/print/scheduler/PrinterConstants.java
@@ -0,0 +1,10 @@
+package com.print.scheduler;
+
+public class PrinterConstants {
+	public static final String PRINT_FILE_FORMAT = "pdf";
+	public static final String PROCESSED_DIRECTORY  = "processed";
+	public static final String INVOICE_DIRECTORY = "invoice";
+	public static final String LABEL_4_6_DIRECTORY = "label_4.6";
+	public static final String LABEL_8_11_DIRECTORY= "label_8.11";
+	
+}
